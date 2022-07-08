d1ca21ee1844fe0df8554f705bbaec33738447c6
Alexandre Bisognin <alexandrefb.86@gmail.com>
2022-07-08 13:50:59 -0300

    Create Global Styles

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { GlobalStyles } from "./styles/global-styles";
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <h1>Teste Grupo Plan</h1>
+    </>
+  );
+}
+
+export default App;
