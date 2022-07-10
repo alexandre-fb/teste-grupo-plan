85b48a47c0c29138b3064c9b7193c45048473d1d
Alexandre Bisognin <alexandrefb.86@gmail.com>
2022-07-10 11:12:42 -0300

    Create "O que fazemos" section

---
+++ src/components/contact-link-card/index.jsx
@@ -0,0 +1,52 @@
+import styled from "styled-components";
+import { Link } from "react-scroll";
+import { FiArrowDownRight as ArrowIcon } from "react-icons/fi";
+import ControlIcon from "../../assets/images/icons/control-icon.png";
+
+export const ContactLinkCard = () => {
+  return (
+    <Container>
+      <img src={ControlIcon} alt="controle game" />
+      <h3>
+        Experimente inovar com a <strong>PlanXP</strong> para{" "}
+        <strong>desenvolver os seus jogos digitais!</strong>
+      </h3>
+      <Link to="contato" smooth spy duration={600}>
+        Entre em contato agora mesmo
+        <ArrowIcon />
+      </Link>
+    </Container>
+  );
+};
+
+export const Container = styled.div`
+  max-width: 160px;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  align-items: start;
+
+  img {
+    width: 50px;
+  }
+
+  h3 {
+    font-size: 20px;
+    line-height: 1.2;
+
+    strong {
+      font-weight: 600;
+    }
+  }
+
+  a {
+    text-transform: uppercase;
+    font-size: 12px;
+    position: relative;
+
+    svg {
+      font-size: 1rem;
+      position: absolute;
+    }
+  }
+`;
--- src/components/sections/o-que-fazemos/index.jsx
+++ src/components/sections/o-que-fazemos/index.jsx
@@ -1,18 +1,92 @@
 import styled from "styled-components";
+import { CardService } from "../../service-card";
+import { ContactLinkCard } from "../../contact-link-card";
+import { SectionContainer } from "../../section-container";
+import { SectionTitle } from "../../section-title";
+import { cardsServicesData, sectionData } from "./sectionData";
+import { screenSizes } from "../../../styles/screen-sizes";
 
 export const OQueFazemosSection = () => {
   return (
-    <Container id='o-que-fazemos'>
-      <div>O QUE FAZEMOS</div>
-    </Container>
+    <SectionContainer id="o-que-fazemos">
+      <Container>
+        <TitleAndDataContainer>
+          <SectionTitle
+            smallTitle="/O que fazemos"
+            bigTitle="/Games"
+            backgroundNumber="02."
+          />
+          <ContainerData>
+            <Title>{sectionData.title}</Title>
+            <SubTitle>{sectionData.subTitle}</SubTitle>
+            <p>{sectionData.text}</p>
+          </ContainerData>
+        </TitleAndDataContainer>
+
+        <CardsServicesAndContactLinkContainer>
+          {cardsServicesData.map((item, index) => {
+            return (
+              <CardService key={index} image={item.image} text={item.text} />
+            );
+          })}
+
+          <ContactLinkCardArea>
+            <ContactLinkCard />
+          </ContactLinkCardArea>
+        </CardsServicesAndContactLinkContainer>
+      </Container>
+    </SectionContainer>
   );
 };
 
-export const Container = styled.section`
-  width: 100%;
-  height: 100vh;
-  background-color: #7a7a7a;
-  color: black;
+export const Container = styled.div`
   display: grid;
-  place-items: center;
+  grid-template-columns: 4fr 5fr;
+  justify-content: space-between;
+  grid-gap: 40px;
+
+  @media (max-width: ${screenSizes.smallLaptop}) {
+    grid-template-columns: 100%;
+  } ;
+`;
+
+export const TitleAndDataContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+export const ContainerData = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+
+export const Title = styled.h2`
+  font-size: 30px;
+  color: var(--medium-gray);
+
+  @media (max-width: ${screenSizes.mobile}) {
+    font-size: 25px;
+  } ;
+`;
+
+export const SubTitle = styled.h3`
+  font-size: 16px;
+  color: var(--medium-gray);
+`;
+
+export const CardsServicesAndContactLinkContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(130px, 1fr));
+  grid-gap: 40px 20px;
+
+  @media (max-width: ${screenSizes.mobile}) {
+    grid-template-columns: 100%;
+    justify-items: center;
+  } ;
+`;
+
+export const ContactLinkCardArea = styled.div`
+  align-self: center;
 `;
